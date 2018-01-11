d3e423cbb047117e5108c837de73e613cb4c7ef7
aletz97 <alessiobertonmail@gmail.com>
2018-01-11 17:52:15 +0100

    redone

---
+++ app/src/main/java/com/example/aletzz/streamdownloadmjpegvideoapp/MjpegContainer.kt
@@ -0,0 +1,5 @@
+package com.example.aletzz.streamdownloadmjpegvideoapp
+
+import android.graphics.Bitmap
+
+class MjpegContainer internal constructor(internal var bitmap: Bitmap, var data: ByteArray)
